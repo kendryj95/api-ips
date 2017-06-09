e3b59389b2dc7b6c990277f76090ab0b6775949a
Miguel Costero <miguel.costero@insignia.com.ve>
2017-06-09 11:13:50 -0400

    webhook bitcoin test

--- payments/stripe/webhook/bitcoin.js
+++ payments/stripe/webhook/bitcoin.js
@@ -2,7 +2,7 @@ const Q = require('q')
 const db = require('../../../config/db')
 const email = require('../../../enviroments/email')
 
-function handleDB (data) {
+function handleDB (id_api_call, status) {
 	const deferred = Q.defer()
 
 	db.pool.ips.getConnection((err, con) => {
@@ -12,20 +12,42 @@ function handleDB (data) {
 
 			con.query(
 				{
-					sql: '',
+					sql: 'SELECT * FROM pagos WHERE id_api_call = ?',
 					timeout: 900		
 				},
 				[
+					id_api_call
 				],
 				(err, results) => {
 					if (err) {
 						deferred.reject(err)
 					} else {
+						let has_error = false
 						results.forEach(o => {
 
-
-
+							con.query(
+								'UPDATE pagos SET estado_pago = ? WHERE id_api_call = ?',
+								[
+									status,
+									id_api_callid
+								],
+								(err, result) => {
+									if (err) {
+										has_error = true
+										deferred.reject(err)
+									}
+								}
+							)
 						})
+
+						if (!has_error) {
+							deferred.resolve({
+								client: {
+									email: results[0].consumidor_email,
+									phone: results[0].consumidor_telefono
+								}
+							})
+						}
 					}
 				}
 			)
@@ -36,10 +58,10 @@ function handleDB (data) {
 	return deferred.promise
 }
 
-function handleNotifications (data) {
+function handleNotificationsByEmail (data) {
 	const deferred = Q.defer()
 
-	email.newAsync(data.mail.to, data.mail.subject, data.mail.template, data.mail.context).then(info => {
+	email.newAsync(data.to, data.subject, data.template, data.context).then(info => {
 		deferred.resolve(`Message ${info.messageId} sent: ${info.response}`)
 	}).catch(err => {
 		deferred.reject(err)
@@ -49,7 +71,33 @@ function handleNotifications (data) {
 }
 
 function handleChargeable (webhook) {
-	console.log(webhook.data.object.id)
+	const status      = 'chargeable',
+				id_api_call = data.webhook.data.object.id,
+	
+
+
+	handleDB(id_api_callid, status).then(result => {
+
+		const email = {
+			to: result.client.email,
+			subject: 'test webhook',
+			template: 'new_pay',
+			context: {
+				email: result.client.email
+			}
+		}
+
+		handleNotificationsByEmail(email).then(r => {
+
+			console.log(r)
+
+		}).catch(err => {
+			console.log(err)
+		})
+
+	}).catch(err => {
+		console.log(err)
+	})
 }
 
 module.exports = webhook => {
