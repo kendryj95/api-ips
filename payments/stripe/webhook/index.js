8686d232de66313a026d817d78798f5744ad9b2a
Miguel Costero <miguel.costero@insignia.com.ve>
2017-06-14 13:00:09 -0400

    capa de seguridad al webhook añadida

--- payments/stripe/webhook/index.js
+++ payments/stripe/webhook/index.js
@@ -9,47 +9,44 @@ module.exports = (req, res) => {
 	try {
 		// Comprobar que la petición provenga de stripe
 		event = stripe.webhooks.constructEvent(payload, sigHeader, endpointSecret)
-
-		if (req.body) {
-			const webhook = req.body
-
-			switch (webhook.data.object.type) {
-				case 'bitcoin':
-					require('./bitcoin')(webhook, `${req.protocol}://${req.get('host')}`)
-				break
-
-				case 'card':
-					require('./card')(webhook)
-				break
-				
-				default:
-					switch (webhook.type) {
-						case 'charge.succeeded':
-							require('./succeeded')(webhook)
-						break
-
-						case 'charge.failed':
-							require('./failed')(webhook)
-						break
-
-						default:
-							console.log('===================================')
-							console.log(webhook)
-							console.log('===================================')
-						break
-					}
-
-				break
-			}
-
-			res.sendStatus(200)
-		} else 
-			// Si no se ha enviado el cuerpo devolvemos error 400 (bad request)
-			res.sendStatus(400)
-
 	} catch (e) {
 		// Invalid payload or signature
 		return response.sendStatus(400)
 	}
 
+	if (req.body) {
+		const webhook = req.body
+
+		switch (webhook.data.object.type) {
+			case 'bitcoin':
+				require('./bitcoin')(webhook, `${req.protocol}://${req.get('host')}`)
+			break
+
+			case 'card':
+				require('./card')(webhook)
+			break
+				
+			default:
+				switch (webhook.type) {
+					case 'charge.succeeded':
+						require('./succeeded')(webhook)
+					break
+
+					case 'charge.failed':
+						require('./failed')(webhook)
+					break
+
+					default:
+						console.log('===================================')
+						console.log(webhook)
+						console.log('===================================')
+					break
+				}
+			break
+		}
+		res.sendStatus(200)
+	} else {
+		// Si no se ha enviado el cuerpo devolvemos error 400 (bad request)
+		res.sendStatus(400)
+	}
 }
