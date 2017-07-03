6f72f83375facb03c55222634e0352b118d0514f
Miguel Costero <miguel.costero@insignia.com.ve>
2017-07-03 14:18:41 -0400

    klzxjdsa

--- config/setup.js
+++ config/setup.js
@@ -1,9 +1,6 @@
 const paypal = require('paypal-rest-sdk')
 const stripe = 'sk_test_Hk47JU23LNp1hB0UtgCnGMNH'
 
-// paypal@insignia.com.ve
-// IMC1205!
-
 paypal.configure({
 	'mode'          : 'sandbox',
 	'client_id'     : 'AYdVxAu3gwaTAcv4qU07-KjHNGS8EnwUEJL0IUxZFz1KWmKZMLeuppa18ddkfq7ZMgigwqVm4_BJOnM5',
